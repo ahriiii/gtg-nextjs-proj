16225af33ea0b08393f8cdbb56b14ee1704525a3
Sinon <47292896+ahriiii@users.noreply.github.com>
2025-02-02 19:41:43 +0800

    Pixel Tracker

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,12 @@ import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
 import Card from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
+import PixelTracker from "@/components/pixelTracker/PixelTracker";
 
 export default function Home() {
   return (
     <div>
+      <PixelTracker />
       <Featured />
       <div className={styles.content}>
         <Card />
